64aa89d1d455a052ade97243b186ca78655bab77
Neil Carberry <ncarberry@mackays.co.uk>
2021-01-21 13:27:36 +0000

    added exception middleware

---
+++ API/Middleware/CustomExceptionHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace API.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class CustomExceptionHandler : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            context.HttpContext.Response.ContentType = "application/json";
+                   
+            var code = HttpStatusCode.InternalServerError;
+
+            if (context.Exception is BaseHttpException ex)
+            {
+                code = ex.Code;
+
+                context.HttpContext.Response.StatusCode = (int)code;
+                context.Result = new JsonResult(new
+                {
+                    type = "Custom_" + ex.Type,
+                    errors = ex.Errors
+                });
+            }
+            else
+            {
+                context.HttpContext.Response.StatusCode = (int)code;
+
+                context.Result = new JsonResult(new
+                {
+                    error = new[] { context.Exception.Message },
+                    innerException = context.Exception.InnerException?.Message,
+                    stackTrace = context.Exception.StackTrace
+                });
+            }
+        }
+    }
+}
---
+++ Application/Abstractions/BaseEventException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public class BaseEventException : Exception
+{
+    protected BaseEventException(string[] errors) : base(errors.FirstOrDefault())
+    {
+        Type = "Exception";
+        Errors = errors;
+    }
+
+    public string[] Errors { get; }
+
+    public string Type { get; }
+}
